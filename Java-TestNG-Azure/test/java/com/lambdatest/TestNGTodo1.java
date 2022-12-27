c516aa2d8aac249a4eebbe11a874bf9214d168de
Kaustubh Dandale <mykaustubh@hotmail.com>
2022-12-27 18:40:33 +0530

    added-buildname

--- Java-TestNG-Azure/test/java/com/lambdatest/TestNGTodo1.java
+++ Java-TestNG-Azure/test/java/com/lambdatest/TestNGTodo1.java
@@ -22,6 +22,7 @@ public class TestNGTodo1 {
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
         String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        String buildname = System.getenv("LT_BUILD_NAME") == null ? "Your LT AccessKey" : System.getenv("LT_BUILD_NAME");
         ;
         String hub = "@hub.lambdatest.com/wd/hub";
 
@@ -29,7 +30,7 @@ public class TestNGTodo1 {
         caps.setCapability("platform", "MacOS Catalina");
         caps.setCapability("browserName", "Safari");
         caps.setCapability("version", "latest");
-        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("build", buildname);
         caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
 
