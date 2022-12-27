d9e405e025618f91bb835016d4fa4eda0e6d6e8b
Kaustubh Dandale <mykaustubh@hotmail.com>
2022-12-27 19:03:31 +0530

    edit-veriables

--- Java-TestNG-Azure/test/java/com/lambdatest/TestNGTodo1.java
+++ Java-TestNG-Azure/test/java/com/lambdatest/TestNGTodo1.java
@@ -20,8 +20,8 @@ public class TestNGTodo1 {
 
     @BeforeMethod
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
-        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
-        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+        String username = System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY");
         String buildname = System.getenv("LT_BUILD_NAME");
 
         String hub = "@hub.lambdatest.com/wd/hub";
